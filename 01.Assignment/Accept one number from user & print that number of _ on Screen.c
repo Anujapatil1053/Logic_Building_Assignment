054a1ece18651b7969326109069c8a2efd3ae474
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-07-20 21:42:13 +0530

    Add files via upload

---
+++ 01.Assignment/Accept one number from user & print that number of _ on Screen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+
+void Accept(int No)
+{
+    int icnt = 0;
+  
+    for( icnt = 1;icnt <= No; icnt++)
+    {
+       
+      printf(" * ");
+    }  
+ 
+}
+int main()
+{
+   int ivalue = 0;
+   
+   ivalue = 5;
+  
+  Accept( ivalue );
+  
+  return 0;
+}
