d88919a0751cdb4fd68ecd44863079ef2c7ecfef
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-07-21 21:14:51 +0530

    Add files via upload

---
+++ 01.Assignment/program to orint 5 to 1 number on screen.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Display()
+{
+  int i=0;
+  
+  i=5;
+  
+   
+  
+  while(i > 0)
+  {
+    printf("\n %d \n",i);
+    i--;
+  }
+}
+int main()
+{
+  Display();
+  return 0;
+}
