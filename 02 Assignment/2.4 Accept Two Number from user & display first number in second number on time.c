7399ad25a76dab126d6a697be388b9f4e45dd881
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-07-29 23:03:34 +0530

    Add files via upload

---
+++ 02 Assignment/2.4 Accept Two Number from user & display first number in second number on time.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+
+void Display(int iNo, int iFrequency)
+{
+  int i = 0;
+  
+  for(i = 1; i <= iFrequency; i++)
+  {
+    printf("\n %d ",iNo);
+  }
+}
+
+int main()
+{
+  int iValue = 0;
+  
+  int iCount = 0;
+  
+  printf("\n Enter A Number =");
+  scanf("%d",&iValue);
+  
+  printf("\n Enter A Frequency =");
+  scanf("%d",&iCount);
+  
+  Display(iValue, iCount);
+  
+  return 0;
+}
