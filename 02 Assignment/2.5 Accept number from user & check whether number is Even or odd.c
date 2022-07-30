61d05b96bae557dd12e4c44a093c7eaeca129898
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-07-30 20:53:58 +0530

    Add files via upload

---
+++ 02 Assignment/2.5 Accept number from user & check whether number is Even or odd.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <conio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+ChkEven ( int iNo )
+{
+  if ( iNo % 2 == 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int main()
+{
+  int iValue = 0 , bRet = 0;
+  
+  
+  printf("\n Enter A Number = ");
+  scanf("%d",&iValue);
+  
+  bRet = ChkEven(iValue);
+  
+  if(bRet == 1)
+  {
+    printf("\n Given Number %d Is Even \n ",iValue );
+  }
+  else
+  {
+    printf("\n Given Number %d Is Odd \n ",iValue);
+  }
+}
