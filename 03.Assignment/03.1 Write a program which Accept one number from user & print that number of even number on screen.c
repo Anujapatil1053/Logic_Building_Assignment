7f687ed8cac9ab2d57f2ff2e0ee57b91112daccc
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-08-01 21:23:16 +0530

    Add files via upload

---
+++ 03.Assignment/03.1 Write a program which Accept one number from user & print that number of even number on screen.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <conio.h>
+
+void printEven(int iNo)
+{
+  
+  
+  if ( iNo <= 0)
+  {
+    return ;
+  }
+  
+  int i = 0;
+  
+  for( i = 1;i <= iNo;i++ )
+  {
+  
+    if ( i % 2 == 0 )
+    {
+      printf("\n %d \n",i);
+    }
+    
+
+  }
+  
+  
+}
+
+int main()
+{
+  int iValue = 0;
+  
+  printf("\n Enter A Number =");
+  scanf("%d",&iValue);
+  
+  printEven(iValue);
+  
+  return 0;
+}
