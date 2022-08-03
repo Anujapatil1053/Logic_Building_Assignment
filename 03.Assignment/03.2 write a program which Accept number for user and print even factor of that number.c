96114411ba7679a354326f28ebcd1f43e3408841
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-08-03 21:11:05 +0530

    Update 03.2 write a program which Accept number for user and print even factor of that number.c

--- 03.Assignment/03.2 write a program which Accept number for user and print even factor of that number.c
+++ 03.Assignment/03.2 write a program which Accept number for user and print even factor of that number.c
@@ -3,29 +3,32 @@
 
 void displayFactor(int iNo)
 {
-  int i = 0;
+           int i = 0;
   
-  if( iNo <= 0)
-  {
-    iNo = -iNo;
-  }
-  for( i = 1; i <= iNo; i++)
-  {
-    if( iNo % i == 0 && i % 2 == 0)
-    {
-      printf(" \n %d \n",i);
-    }
-  }
-}
+           if( iNo <= 0)
+           {
+                    iNo = -iNo;
+           }
+
+          for( i = 1; i <= iNo; i++)
+          {
+                   if( iNo % i == 0 && i % 2 == 0)
+                   {
+
+                     printf(" \n %d \n",i);
+
+                   }
+          }
+} 
 
 int main()
 {
-  int iValue = 0;
+              int iValue = 0;
   
-  printf("\n Enter A Number =");
-  scanf("%d",&iValue);
+              printf("\n Enter A Number =");
+              scanf("%d",&iValue);
+
+              displayFactor(iValue);
   
-  displayFactor(iValue);
-  
-  return 0;
+              return 0;
 }
