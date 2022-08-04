7bff7ff34b501658d39b92944122bd12af2c0e18
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-08-04 12:40:35 +0530

    Update program to print 5 times Marvellous on screen.c

--- 01.Assignment/program to print 5 times Marvellous on screen.c
+++ 01.Assignment/program to print 5 times Marvellous on screen.c
@@ -4,16 +4,16 @@
 
 void Display()
 {
-          int i=0;
-  
-          for( i = 1;i <= 5; i++)
-          {
-                  printf(" Marvellous\n ");
-          }
+                    int i=0;
+
+                    for( i = 1;i <= 5; i++)
+                    {
+                            printf(" Marvellous\n ");
+                    }
 }
 int main()
 {
-          Display();
-          
-          return 0;
+                    Display();
+
+                    return 0;
 }
