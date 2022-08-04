9d2dd0db1e09357496d02b4ee38d8a529a6cd90e
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-08-04 12:44:26 +0530

    Update 2.4 Accept Two Number from user & display first number in second number on time.c

--- 02 Assignment/2.4 Accept Two Number from user & display first number in second number on time.c
+++ 02 Assignment/2.4 Accept Two Number from user & display first number in second number on time.c
@@ -3,27 +3,27 @@
 
 void Display(int iNo, int iFrequency)
 {
-              int i = 0;
-  
-              for(i = 1; i <= iFrequency; i++)
-              {
-                           printf("\n %d ",iNo);
-              }
+                          int i = 0;
+
+                          for(i = 1; i <= iFrequency; i++)
+                          {
+                                       printf("\n %d ",iNo);
+                          }
 }
 
 int main()
 {
-             int iValue = 0;
-  
-             int iCount = 0;
-  
-             printf("\n Enter A Number =");
-             scanf("%d",&iValue);
-  
-             printf("\n Enter A Frequency =");
-             scanf("%d",&iCount);
-  
-             Display(iValue, iCount);
-   
-             return 0;
+                         int iValue = 0;
+
+                         int iCount = 0;
+
+                         printf("\n Enter A Number =");
+                         scanf("%d",&iValue);
+
+                         printf("\n Enter A Frequency =");
+                         scanf("%d",&iCount);
+
+                         Display(iValue, iCount);
+
+                         return 0;
 }
