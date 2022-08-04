ec0776b561ac7ba50335a4760c2ccf360f006d97
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-08-04 12:39:10 +0530

    Update program to divide two number.c

--- 01.Assignment/program to divide two number.c
+++ 01.Assignment/program to divide two number.c
@@ -3,26 +3,26 @@
 
 int Divide( int iNo1, int iNo2 )
 {
-                int iAns = 0;
+                  int iAns = 0;
 
-                if( iNo2 > iNo1)
-                {
-                     return -1;
-                }
+                  if( iNo2 > iNo1)
+                  {
+                       return -1;
+                  }
 
-                iAns = iNo1 / iNo2;
+                  iAns = iNo1 / iNo2;
 
-                return iAns;
-}
+                  return iAns;
+  }
 
 int main()
 {
-                int iValue1 = 15, iValue2 = 5;
-                int iRet = 0;
+                  int iValue1 = 15, iValue2 = 5;
+                  int iRet = 0;
 
-                iRet = Divide( iValue1, iValue2 );
+                  iRet = Divide( iValue1, iValue2 );
 
-                printf("\n Division Is : %d",iRet);
+                  printf("\n Division Is : %d",iRet);
 
-                return 0;
-}
+                  return 0;
+  }
