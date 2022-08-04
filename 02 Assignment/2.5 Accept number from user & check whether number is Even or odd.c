46997b77932a666baabd67723385e4980a029664
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-08-04 12:45:51 +0530

    Update 2.5 Accept number from user & check whether number is Even or odd.c

--- 02 Assignment/2.5 Accept number from user & check whether number is Even or odd.c
+++ 02 Assignment/2.5 Accept number from user & check whether number is Even or odd.c
@@ -6,34 +6,34 @@
 
 ChkEven ( int iNo )
 {
-           if ( iNo % 2 == 0 )
-           {
-                     return 1;
-           }
+                                            if ( iNo % 2 == 0 )
+                                            {
+                                                      return 1;
+                                            }
 
-           else
-           {
-                     return 0;
-           }
+                                            else
+                                            {
+                                                      return 0;
+                                            }
 }
 
 int main()
 {
-           int iValue = 0 , bRet = 0;
-  
-  
-          Printf("\n Enter A Number = ");
-          scanf("%d",&iValue);
-  
-          bRet = ChkEven(iValue);
-  
-          if(bRet == 1)
-          {
-                    printf("\n Given Number %d Is Even \n ",iValue );
-          }
+                                            int iValue = 0 , bRet = 0;
 
-          else
-          {
-                    printf("\n Given Number %d Is Odd \n ",iValue);
-          }
+
+                                           Printf("\n Enter A Number = ");
+                                           scanf("%d",&iValue);
+
+                                           bRet = ChkEven(iValue);
+
+                                           if(bRet == 1)
+                                           {
+                                                     printf("\n Given Number %d Is Even \n ",iValue );
+                                           }
+
+                                           else
+                                           {
+                                                     printf("\n Given Number %d Is Odd \n ",iValue);
+                                           }
 }
