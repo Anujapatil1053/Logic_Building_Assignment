1646396f879e26fc06ffd2516e2a63ffa7010785
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-08-04 12:48:58 +0530

    Update 03.1 Write a program which Accept one number from user & print that number of even number on screen.c

--- 03.Assignment/03.1 Write a program which Accept one number from user & print that number of even number on screen.c
+++ 03.Assignment/03.1 Write a program which Accept one number from user & print that number of even number on screen.c
@@ -5,35 +5,35 @@ void printEven(int iNo)
 {
   
   
-           if ( iNo <= 0)
-           {
-                   return ;
-           }
-  
-           int i = 0;
-  
-           for( i = 1;i <= iNo;i++ )
-           {
-  
-           if ( i % 2 == 0 )
-           {
-                    printf("\n %d \n",i);
-           }
+                   if ( iNo <= 0)
+                   {
+                           return ;
+                   }
+
+                   int i = 0;
+
+                   for( i = 1;i <= iNo;i++ )
+                   {
+
+                   if ( i % 2 == 0 )
+                   {
+                            printf("\n %d \n",i);
+                   }
     
 
-  }
+                  }
   
   
 }
 
 int main()
 {
-           int iValue = 0;
-  
-           printf("\n Enter A Number =");
-           scanf("%d",&iValue);
-  
-           printEven(iValue);
-  
-           return 0;
+                 int iValue = 0;
+
+                 printf("\n Enter A Number =");
+                 scanf("%d",&iValue);
+
+                 printEven(iValue);
+
+                 return 0;
 }
