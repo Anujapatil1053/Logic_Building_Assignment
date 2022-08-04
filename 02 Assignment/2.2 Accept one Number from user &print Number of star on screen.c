85ed519a42178539f74a77c6549b0a1c9be4dd41
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-08-04 12:41:49 +0530

    Update 2.2 Accept one Number from user &print Number of star on screen.c

--- 02 Assignment/2.2 Accept one Number from user &print Number of star on screen.c
+++ 02 Assignment/2.2 Accept one Number from user &print Number of star on screen.c
@@ -3,25 +3,25 @@
 
 void Display( int iNo)
 {
-  
-              int i = 0;
-  
-              while( iNo > i)
-              {
-                       printf("\t * \n");
 
-                       iNo--;
-              }
+                        int i = 0;
+
+                        while( iNo > i)
+                        {
+                                 printf("\t * \n");
+
+                                   iNo--;
+                        }
 }
 
 int main()
 {
-               int iValue = 0;
-  
-               printf("\n Enter Number = ");
-               scanf("%d",&iValue);
-  
-               Display(iValue);
-  
-               return 0;
+                         int iValue = 0;
+
+                         printf("\n Enter Number = ");
+                         scanf("%d",&iValue);
+
+                         Display(iValue);
+
+                         return 0;
 }
