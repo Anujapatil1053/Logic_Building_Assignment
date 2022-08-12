a04bb069e13fc188d1664bf399ad8df01dc5ce22
Anujapatil1053 <107532915+Anujapatil1053@users.noreply.github.com>
2022-08-12 20:52:29 +0530

    Add files via upload

---
+++ 05 Assignment/05_2 Write A Program Which Accept Number From User And Display Its Factor in Decreasing.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <conio.h>
+
+void FactRev ( int iNo )
+{
+    int i = 0, Rev = 0, Ans = 0;
+    
+    if( iNo <= 0)
+    {
+      return;
+    }
+    
+    for( i = iNo ; i > 0 ; i-- )
+    {
+      if( iNo % i == 0 && iNo != i)
+      {
+        Ans = i;
+      }
+      
+      printf( "\n %d \n ",Ans );
+    }
+}
+
+int main()
+{
+  int iValue =0;
+  
+  printf( " \n Enter A Number = ");
+  scanf( "%d",&iValue );
+  
+  FactRev( iValue );
+  
+  return 0;
+}
